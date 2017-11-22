2d22a6f25d1d8d5572d3d07568481f8c113d9bac
HiRobot <hi_robot@126.com>
2017-11-22 10:45:26 +0800

    add health  check

--- src/jplat/service/trans/test/HealthCheckerTrans.java
+++ src/jplat/service/trans/test/HealthCheckerTrans.java
@@ -1,23 +1,15 @@
 package jplat.service.trans.test;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import jplat.core.trans.JAppContext;
 import jplat.service.ctl.test.model.HealthReqModel;
 import jplat.service.ctl.test.model.HealthRspModel;
 import jplat.tools.config.JAppConfig;
 import jplat.tools.string.JStringUtil;
-import jplat.tools.string.StringUtil;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Component
 //@Controller
@@ -34,18 +26,4 @@ public class HealthCheckerTrans
 		rspModel.setRetMsg("pong");
 		rspModel.setYourMsg(reqModel.getMsg());
 	}
-	
-	@RequestMapping("/test/ping1.do")
-	@ResponseBody
-	public Map<String,String> healthCheck( HttpServletRequest request, HttpServletResponse response )
-	{
-		Map<String,String> retMap = new HashMap<String,String>();
-		retMap.put("msg", "pong");
-		retMap.put("ipAddr", StringUtil.getLocalIP());
-		retMap.put("appName", request.getContextPath() );
-
-//		RuntimeMXBean bean = ManagementFactory.getRuntimeMXBean();
-		return retMap;
-	}
-	
 }
