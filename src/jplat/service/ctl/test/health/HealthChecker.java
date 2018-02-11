45c11f575093846d4a84198bc02f0954d11a08ee
hi_robot <hi_robot@126.com>
2018-02-11 11:57:26 +0800

    优化日志

--- src/jplat/service/ctl/test/health/HealthChecker.java
+++ src/jplat/service/ctl/test/health/HealthChecker.java
@@ -14,6 +14,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
